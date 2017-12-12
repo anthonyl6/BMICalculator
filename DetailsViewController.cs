6abbcb6b2e055f0da831d85cb596fe14b71579ac
anthonyl6 <anthonyl6@learn.cssd.ab.ca>
2017-12-12 14:33:10 -0700

    updated added a table view

---
+++ DetailsViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace BMICalculator
+{
+    public partial class DetailsViewController.cs : UITableViewCell
+    {
+        public DetailsViewController.cs (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
---
+++ DetailsViewController.cs.designer.cs
@@ -0,0 +1,29 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace BMICalculator
+{
+    [Register ("DetailsViewController.cs")]
+    partial class DetailsViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel labelDetailsResult { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (labelDetailsResult != null) {
+                labelDetailsResult.Dispose ();
+                labelDetailsResult = null;
+            }
+        }
+    }
+}
